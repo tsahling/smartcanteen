ed343d2b544ef183c247537890c79eee300042ad
tim.sahling@gmail.com <tim.sahling@gmail.com@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2012-12-17 10:21:22 +0000

    CSVTokenizer bearbeitet mit URL

--- src/main/java/de/osjava/smartcanteen/application/Application.java
+++ src/main/java/de/osjava/smartcanteen/application/Application.java
@@ -1,6 +1,7 @@
 package de.osjava.smartcanteen.application;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Vector;
 import java.util.logging.Logger;
@@ -71,99 +72,75 @@ public class Application {
     /**
      * 
      * @param args
+     * @throws IOException
      */
-    private boolean initInput(final String[] args) {
-		String inputFiles = EMPTY;
+    private boolean initInput(final String[] args) throws IOException {
+        String inputFiles = EMPTY;
 
-		if (args.length == 0) {
-			inputFiles = JOptionPane.showInputDialog(null, PropertyHelper
-					.getProperty("message.missingInputFiles.message"),
-					PropertyHelper
-							.getProperty("message.missingInputFiles.title"),
-					JOptionPane.QUESTION_MESSAGE);
-		} else if (args.length == 1
-				&& args[0].equals(PropertyHelper.getProperty("param.help"))) {
-			System.out.println(PropertyHelper
-					.getProperty("application.usageInfo"));
-			return false;
-		} else {
-			for (String arg : args) {
+        if (args.length == 0) {
+            inputFiles = JOptionPane.showInputDialog(null, PropertyHelper
+                    .getProperty("message.missingInputFiles.message"),
+                    PropertyHelper
+                            .getProperty("message.missingInputFiles.title"),
+                    JOptionPane.QUESTION_MESSAGE);
+        }
+        else if (args.length == 1
+                && args[0].equals(PropertyHelper.getProperty("param.help"))) {
+            System.out.println(PropertyHelper
+                    .getProperty("application.usageInfo"));
+            return false;
+        }
+        else {
+            for (String arg : args) {
 
-				if (arg.contains(PropertyHelper.getProperty("param.inputFiles"))) {
-					String[] argSplit = arg.split(ARG_SPLIT);
+                if (arg.contains(PropertyHelper.getProperty("param.inputFiles"))) {
+                    String[] argSplit = arg.split(ARG_SPLIT);
 
-					if (argSplit.length > 1) {
-						inputFiles = inputFiles.concat(argSplit[1].trim())
-								.concat(INPUT_FILE_SPLIT);
-					}
-				}
-			}
-		}
+                    if (argSplit.length > 1) {
+                        inputFiles = inputFiles.concat(argSplit[1].trim())
+                                .concat(INPUT_FILE_SPLIT);
+                    }
+                }
+            }
+        }
 
-		boolean wrongInputFile = false;
+        boolean wrongInputFile = false;
 
-		if (inputFiles != null && !inputFiles.equals(EMPTY)) {
-			String[] inputFileSplit = inputFiles.split(INPUT_FILE_SPLIT);
+        if (inputFiles != null && !inputFiles.equals(EMPTY)) {
+            String[] inputFileSplit = inputFiles.split(INPUT_FILE_SPLIT);
 
-			if (inputFileSplit.length > 0) {
-				for (String inputFile : inputFileSplit) {
+            if (inputFileSplit.length > 0) {
+                for (String inputFile : inputFileSplit) {
 
-					URL inputFileUrl = ClassLoader
-							.getSystemResource(PropertyHelper
-									.getProperty("application.inputFilePath")
-									+ inputFile);
+                    URL inputFileUrl = ClassLoader
+                            .getSystemResource(PropertyHelper
+                                    .getProperty("application.inputFilePath")
+                                    + inputFile);
 
-					if (inputFileUrl != null) {
-						// TODO(Francesco Luciano) Einlesen der Dateien aus
-						// Dateisystem in die Verwaltungsklassen und erstellen
-						// der Verwaltungsklassen und zuweisen an
-						// Klassenattribute
-					    
-					    public HitListBase readHitlist() {
+                    if (inputFileUrl != null) {
+                        // TODO(Francesco Luciano) Einlesen der Dateien aus
+                        // Dateisystem in die Verwaltungsklassen und erstellen
+                        // der Verwaltungsklassen und zuweisen an
+                        // Klassenattribute
+                        readHitlist(inputFileUrl);
 
-					        Vector<String[]> lines = new Vector<String[]>();
-					        HitListBase hitlist = new HitListBase();
-					        Integer mealPlacement = null;
-					        String mealName = null;
+                    }
+                    else {
+                        wrongInputFile = true;
+                        break;
+                    }
+                }
 
-					        try {
-					            CSVTokenizer csv = new CSVTokenizer(
-					                    "/Users/frato/Desktop/hitliste.csv", ',');
+                if (!wrongInputFile) {
+                    return true;
+                }
+            }
+        }
 
-					            while (csv.hasMoreLines()) {
-					                lines.add(csv.nextLine());
-					            }
-					            for (int i = 0; i <= lines.size() - 1; i++) {
-
-					                mealPlacement = Integer.valueOf(lines.get(i)[0]);
-					                mealName = lines.get(i)[1];
-					                HitListItem oneHitListItem = new HitListItem(mealName, mealPlacement);
-					                hitlist.addHitListItem(oneHitListItem);
-					            }
-					        } catch (FileNotFoundException e) {
-					            e.printStackTrace();
-					        } catch (ParseException e) {
-					            e.printStackTrace();
-					        }
-					        return hitlist;
-					    }
-					    
-					} else {
-						wrongInputFile = true;
-						break;
-					}
-				}
-
-				if (!wrongInputFile) {
-					return true;
-				}
-			}
-		}
-
-		throw new IllegalArgumentException(
-				PropertyHelper
-						.getProperty("message.missingInputFiles.exception"));
-	}
+        throw new IllegalArgumentException(
+                PropertyHelper
+                        .getProperty("message.missingInputFiles.exception"));
+    }
 
     /**
      * 
@@ -184,5 +161,33 @@ public class Application {
             Canteen... canteens) {
         // TODO(Marcel Baxmann) Ergebnisse der Applikationslogik mit
         // Output-Klassen verarbeiten
+    }
+
+    public HitListBase readHitlist(URL inputFileURL) throws IOException {
+
+        Vector<String[]> lines = new Vector<String[]>();
+        HitListBase hitlist = new HitListBase();
+        Integer mealPlacement = null;
+        String mealName = null;
+
+        try {
+            CSVTokenizer csv = new CSVTokenizer(inputFileURL, ',');
+
+            while (csv.hasMoreLines()) {
+                lines.add(csv.nextLine());
+            }
+            for (int i = 0; i <= lines.size() - 1; i++) {
+
+                mealPlacement = Integer.valueOf(lines.get(i)[0]);
+                mealName = lines.get(i)[1];
+                HitListItem oneHitListItem = new HitListItem(mealName, mealPlacement);
+                hitlist.addHitListItem(oneHitListItem);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return hitlist;
     }
 }
