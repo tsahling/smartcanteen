ffa76c55e7a9e32741d34a60db30b65ba31bd241
tim.sahling@gmail.com <tim.sahling@gmail.com@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2012-12-18 10:36:14 +0000

    Formatierung der ENUMs verbessert

--- trunk/src/main/java/de/osjava/smartcanteen/datatype/UnitOfMeasurement.java
+++ trunk/src/main/java/de/osjava/smartcanteen/datatype/UnitOfMeasurement.java
@@ -11,56 +11,61 @@ package de.osjava.smartcanteen.datatype;
  */
 public enum UnitOfMeasurement {
 
-	GRM("GRM", "Gramm", "g"), KGM("KGM", "Kilogramm", "kg"), TON("TON",
-			"Tonne", "t"), MTR("MTR", "Meter", "m"), KMR("KMR", "Kilometer",
-			"km"), MLR("MLR", "Milliliter", "ml"), LTR("LTR", "Liter", "l"), STK(
-			"STK", "Stueck", "stk"), EUR("EUR", "Euro", "Eur");
+    GRM("GRM", "Gramm", "g"),
+    KGM("KGM", "Kilogramm", "kg"),
+    TON("TON", "Tonne", "t"),
+    MTR("MTR", "Meter", "m"),
+    KMR("KMR", "Kilometer", "km"),
+    MLR("MLR", "Milliliter", "ml"),
+    LTR("LTR", "Liter", "l"),
+    STK("STK", "Stueck", "stk"),
+    EUR("EUR", "Euro", "Eur");
 
-	private final String code;
-	private final String name;
-	private final String shortName;
+    private final String code;
+    private final String name;
+    private final String shortName;
 
-	/**
-	 * Im Standardkonstruktor werden dem Enum {@link UnitOfMeasurement}
-	 * Variablen übergeben, die final gespeichert werden.
-	 * 
-	 * @param code
-	 *            Deklaration des ENUM z.B. KGM
-	 * @param name
-	 *            Beschreibung des ENUM z.B. Kilogramm
-	 * @param shortName
-	 *            Kurzbeschreibung des ENUM z.B. kg
-	 */
-	private UnitOfMeasurement(String code, String name, String shortName) {
-		this.code = code;
-		this.name = name;
-		this.shortName = shortName;
-	}
+    /**
+     * Im Standardkonstruktor werden dem Enum {@link UnitOfMeasurement} Variablen übergeben, die final gespeichert
+     * werden.
+     * 
+     * @param code
+     *            Deklaration des ENUM z.B. KGM
+     * @param name
+     *            Beschreibung des ENUM z.B. Kilogramm
+     * @param shortName
+     *            Kurzbeschreibung des ENUM z.B. kg
+     */
+    private UnitOfMeasurement(String code, String name, String shortName) {
+        this.code = code;
+        this.name = name;
+        this.shortName = shortName;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts Code
-	 * 
-	 * @return code Deklaration
-	 */
-	public String getCode() {
-		return code;
-	}
+    /**
+     * Methode zum Abrufen des Werts Code
+     * 
+     * @return code Deklaration
+     */
+    public String getCode() {
+        return code;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts Name
-	 * 
-	 * @return name Beschreibung
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Methode zum Abrufen des Werts Name
+     * 
+     * @return name Beschreibung
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts ShortName
-	 * 
-	 * @return shortName Kurzbeschreibung
-	 */
-	public String getShortName() {
-		return shortName;
-	}
+    /**
+     * Methode zum Abrufen des Werts ShortName
+     * 
+     * @return shortName Kurzbeschreibung
+     */
+    public String getShortName() {
+        return shortName;
+    }
 }
