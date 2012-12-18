e0fac311746641f4d7238ccfd4f292d6eae67560
tim.sahling@gmail.com <tim.sahling@gmail.com@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2012-12-18 10:36:14 +0000

    Formatierung der ENUMs verbessert

--- src/main/java/de/osjava/smartcanteen/datatype/CanteenLocation.java
+++ src/main/java/de/osjava/smartcanteen/datatype/CanteenLocation.java
@@ -13,81 +13,81 @@ import de.osjava.smartcanteen.data.Canteen;
  */
 public enum CanteenLocation {
 
-	ESSEN("ESSEN", "Essen", "Essen", 51.455644, 7.011554), MUELHEIM("MUELHEIM",
-			"Muelheim an der Ruhr", "Muelheim", 51.430958, 6.880744);
+    ESSEN("ESSEN", "Essen", "Essen", 51.455644, 7.011554),
+    MUELHEIM("MUELHEIM", "Muelheim an der Ruhr", "Muelheim", 51.430958, 6.880744);
 
-	private final String code;
-	private final String name;
-	private final String shortName;
-	private final double latitude;
-	private final double longitude;
+    private final String code;
+    private final String name;
+    private final String shortName;
+    private final double latitude;
+    private final double longitude;
 
-	/**
-	 * Im Standardkonstruktor werden dem Enum {@link CanteenLocation} Variablen
-	 * übergeben, die final gespeichert werden.
-	 * 
-	 * @param code
-	 *            Deklaration des ENUM z.B. ESSEN
-	 * @param name
-	 *            Beschreibung des ENUM z.B. Stadt Essen
-	 * @param shortName
-	 *            Kurzbeschreibung des ENUM z.B. Essen
-	 * @param latitude
-	 *            Breitengrad der Ortes
-	 * @param longitude
-	 *            Längengrad des Ortes
-	 */
-	private CanteenLocation(String code, String name, String shortName,
-			double latitude, double longitude) {
-		this.code = code;
-		this.name = name;
-		this.shortName = shortName;
-		this.latitude = latitude;
-		this.longitude = longitude;
-	}
+    /**
+     * Im Standardkonstruktor werden dem Enum {@link CanteenLocation} Variablen
+     * übergeben, die final gespeichert werden.
+     * 
+     * @param code
+     *            Deklaration des ENUM z.B. ESSEN
+     * @param name
+     *            Beschreibung des ENUM z.B. Stadt Essen
+     * @param shortName
+     *            Kurzbeschreibung des ENUM z.B. Essen
+     * @param latitude
+     *            Breitengrad der Ortes
+     * @param longitude
+     *            Längengrad des Ortes
+     */
+    private CanteenLocation(String code, String name, String shortName,
+            double latitude, double longitude) {
+        this.code = code;
+        this.name = name;
+        this.shortName = shortName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts Code
-	 * 
-	 * @return code Deklaration
-	 */
-	public String getCode() {
-		return code;
-	}
+    /**
+     * Methode zum Abrufen des Werts Code
+     * 
+     * @return code Deklaration
+     */
+    public String getCode() {
+        return code;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts Name
-	 * 
-	 * @return name Beschreibung
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Methode zum Abrufen des Werts Name
+     * 
+     * @return name Beschreibung
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts ShortName
-	 * 
-	 * @return shortName Kurzbeschreibung
-	 */
-	public String getShortName() {
-		return shortName;
-	}
+    /**
+     * Methode zum Abrufen des Werts ShortName
+     * 
+     * @return shortName Kurzbeschreibung
+     */
+    public String getShortName() {
+        return shortName;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts Latitude
-	 * 
-	 * @return latitude Breitengrad
-	 */
-	public double getLatitude() {
-		return latitude;
-	}
+    /**
+     * Methode zum Abrufen des Werts Latitude
+     * 
+     * @return latitude Breitengrad
+     */
+    public double getLatitude() {
+        return latitude;
+    }
 
-	/**
-	 * Methode zum Abrufen des Werts Longitude
-	 * 
-	 * @return longitude Laengengrad
-	 */
-	public double getLongitude() {
-		return longitude;
-	}
+    /**
+     * Methode zum Abrufen des Werts Longitude
+     * 
+     * @return longitude Laengengrad
+     */
+    public double getLongitude() {
+        return longitude;
+    }
 }
