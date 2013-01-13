e14ffc8e89f08bdb09dc6294d3dee623c1c7dcb5
tim.sahling@gmail.com <tim.sahling@gmail.com@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2013-01-13 21:57:39 +0000

    Start Ausgabe der Ergebnisse

--- trunk/src/main/java/de/osjava/smartcanteen/output/FileOutput.java
+++ trunk/src/main/java/de/osjava/smartcanteen/output/FileOutput.java
@@ -3,7 +3,13 @@ package de.osjava.smartcanteen.output;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import de.osjava.smartcanteen.builder.result.Meal;
 import de.osjava.smartcanteen.builder.result.ShoppingList;
 import de.osjava.smartcanteen.data.Canteen;
 
@@ -39,14 +45,55 @@ public class FileOutput implements IOutput {
 
     @Override
     public void outputMenuPlan(Canteen canteen) {
-        System.out.println(canteen.getMenuPlan().getMealsGroupedByDate());
-        try {
-            ausgebenInDatei("Die Daten die in die Datei geschrieben werden sollen als Objekt");
-        } catch (IOException e) {
-            // TODO(Marcel) handle this exception properly
-            e.printStackTrace();
+        // <<<<<<< .mine
+
+        List<Meal> meals = canteen.getMenuPlan().getMeals();
+
+        Map<Date, List<Meal>> mealsGroupedByDate = canteen.getMenuPlan().getMealsGroupedByDate();
+
+        for (Entry<Date, List<Meal>> entry : mealsGroupedByDate.entrySet()) {
+
+            entry.getKey();
+
+            List<Meal> value = entry.getValue();
+
+            for (Meal xyz : value) {
+
+                xyz.getRecipe().getName();
+
+                String transformDateToNiceString = transformDateToNiceString(xyz.getDate());
+
+            }
+
         }
 
+        if (true) {
+            // throw new IOException(PropertyHelper.getProperty(""));
+        }
+
+        Set<Date> keySet = mealsGroupedByDate.keySet();
+
+        for (Date xyz : keySet) {
+
+            System.out.println(xyz.toString());
+
+        }
+
+        System.out.println(canteen.getMenuPlan().getMeals());
+        // =======
+        // System.out.println(canteen.getMenuPlan().getMealsGroupedByDate());
+        // try {
+        // ausgebenInDatei("Die Daten die in die Datei geschrieben werden sollen als Objekt");
+        // } catch (IOException e) {
+        // // TODO(Marcel) handle this exception properly
+        // e.printStackTrace();
+        // }
+        // >>>>>>> .r154
+
+    }
+
+    private String transformDateToNiceString(Date date) {
+        return "";
     }
 
     @Override
