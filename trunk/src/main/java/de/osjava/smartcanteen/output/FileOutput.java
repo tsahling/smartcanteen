69c83f92ded4eb982d4e82435f2115308d94680d
marcel.baxmann <marcel.baxmann@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2013-01-17 20:28:04 +0000

    zweite Variante für Speiseplan Ausgabe ergänzt

--- trunk/src/main/java/de/osjava/smartcanteen/output/FileOutput.java
+++ trunk/src/main/java/de/osjava/smartcanteen/output/FileOutput.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import de.osjava.smartcanteen.builder.result.Meal;
 import de.osjava.smartcanteen.builder.result.ShoppingList;
+import de.osjava.smartcanteen.builder.result.ShoppingListItem;
 import de.osjava.smartcanteen.data.Canteen;
 import de.osjava.smartcanteen.helper.PropertyHelper;
 
@@ -38,21 +39,68 @@ public class FileOutput implements IOutput {
     public void outputMenuPlan(Canteen canteen) throws IOException {
         // auslesen des Kantinen-Namens
         String canteenName = canteen.getLocation().getName();
+        // zuweisen der Variante der Datenaufbereitung
+        // wenn 0 --> dann zweispaltige Auflistung nach Namen
+        // wenn 1 --> dann pro Spalte bzw. Datum drei Gerichte
+        int variante = 1;
+        String startDate = null;
 
+        // Erzeugen String-Puffer Objekts in welches die Ergebnisse geschrieben werden
         StringBuilder ausgabeDaten = new StringBuilder();
+
+        // List erstellen, in der die Gerichte nach Datum 1zu1 zugeordnet sind
         List<Meal> mealsSortedByDate = canteen.getMenuPlan().getMealsSortedByDate();
 
         // das Datum des Gültigkeitsbeginns auslesen - Verwendung für Dateiname
-        String startDate = shortendDate(mealsSortedByDate.get(0).getDate());
+        startDate = shortendDate(mealsSortedByDate.get(0).getDate());
 
-        for (Meal sortedMeal : mealsSortedByDate) {
-            String meal;
-            String date;
+        String meal;
+        String date;
 
-            date = shortendDate(sortedMeal.getDate());
-            meal = sortedMeal.getRecipe().getName();
+        if (variante == 0) {
+            // Überschriften Zeile einfügen
+            ausgabeDaten.append("Datum;Gericht" + lineSeparator);
 
-            ausgabeDaten.append(date + ";" + meal + "; " + lineSeparator);
+            // für jedes Gericht (meal) in der Liste werden das Datum und der Namen
+            // ausgelesen und in den String-Puffer angehangen
+            for (Meal sortedMeal : mealsSortedByDate) {
+
+                date = shortendDate(sortedMeal.getDate());
+                meal = sortedMeal.getRecipe().getName();
+
+                ausgabeDaten.append(date + ";" + meal + lineSeparator);
+            }
+        }
+        else {
+            // TODO (Marcel Baxmann) Kommentieren
+            int mealsPerDay = Integer.parseInt(PropertyHelper.getProperty("planingPeriod.mealsPerDay"));
+
+            ausgabeDaten.append("Datum;");
+            for (int i = 1; i < mealsPerDay + 1; i++) {
+                ausgabeDaten.append("Gericht " + i + ";");
+            }
+            ausgabeDaten.append(lineSeparator);
+
+            String previousDate = null;
+            for (Meal sortedMeal : mealsSortedByDate) {
+                date = shortendDate(sortedMeal.getDate());
+                meal = sortedMeal.getRecipe().getName();
+
+                if (previousDate == null) {
+                    ausgabeDaten.append(date + ";" + meal);
+                    previousDate = date;
+                }
+                else {
+                    if (date.equals(previousDate)) {
+                        ausgabeDaten.append(";" + meal);
+                    }
+                    else {
+                        ausgabeDaten.append(lineSeparator);
+                        ausgabeDaten.append(date + ";" + meal);
+                        previousDate = date;
+                    }
+                }
+            }
 
         }
 
@@ -76,6 +124,9 @@ public class FileOutput implements IOutput {
 
     @Override
     public void outputShoppingList(ShoppingList shoppingList) {
+
+        List<ShoppingListItem> shoppingListItems = shoppingList.getShoppingListItems();
+
     }
 
     @Override
@@ -83,12 +134,14 @@ public class FileOutput implements IOutput {
     }
 
     /**
-     * es wird der Dateiname generiert
+     * Es wird der Dateiname generiert, hierbei wird geprüft, ob bereits eine Datei mit dem Dateinamen
+     * existiert. Ist dies der Fall wird solanger der Dateiname hochgezählt,
+     * bis die Datei unter einem neuen Dateinamen gespeichert werden kann *
      * 
      * @return
      */
     public String generateFilename(String customName) {
-        // TODO (Marcel Baxmann) Prüfung vornehmen
+        // TODO(Marcel Baxmann) Prüfung vornehmen
         // auslesen der Pfadangabe
         String path = PropertyHelper.getProperty("outputData.saveTo");
         // setzen der übergebenen Parameter für den Filenamen
@@ -124,6 +177,7 @@ public class FileOutput implements IOutput {
      * @throws IOException
      */
     public void ausgebenInDatei(String ausgabeDaten, String dateiname, boolean anhaengen) throws IOException {
+        // TODO (Marcel Baxmann) kommentieren und auf Fehler prüfen
         File file = new File(dateiname);
         FileWriter writer = new FileWriter(file, anhaengen);
 
