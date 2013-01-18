4b0156e17f30d1c23e0ad68788401d65806b4e40
marcel.baxmann <marcel.baxmann@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2013-01-18 12:53:22 +0000

    - Bilder in HTML Menueplan ergänzt (Stringgenerierung in FileHelper)  - Prüfung ob Bilder vorhanden sind als  Methode im Filehelper  - diverse Kommentierung

--- trunk/src/main/java/de/osjava/smartcanteen/output/FileOutput.java
+++ trunk/src/main/java/de/osjava/smartcanteen/output/FileOutput.java
@@ -1,36 +1,41 @@
 package de.osjava.smartcanteen.output;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import de.osjava.smartcanteen.builder.result.Meal;
 import de.osjava.smartcanteen.builder.result.ShoppingList;
 import de.osjava.smartcanteen.builder.result.ShoppingListItem;
+import de.osjava.smartcanteen.data.AbstractProvider;
 import de.osjava.smartcanteen.data.Canteen;
 import de.osjava.smartcanteen.helper.FileHelper;
 import de.osjava.smartcanteen.helper.PropertyHelper;
 
 /**
- * Die Klasse {@link FileOutput} ist eine Klasse, die das Interface {@link IOutput} implementiert und stellt daher die
- * Methoden die im Interface
- * definiert sind bereit. Die Ausgabe erfolgt in eine Datei im Dateisystem.
+ * Die Klasse {@link FileOutput} ist eine Klasse, die das Interface {@link IOutput} implementiert
+ * und stellt daher die Methoden die im Interface definiert sind bereit.
+ * Die Ausgabe erfolgt in Dateien im Dateisystem als CSV. Der Pfad für den Ablageort
+ * wird in der zentralen Properties verwaltet.
  * 
  * @author Marcel Baxmann
  */
 public class FileOutput implements IOutput {
     // Auslesen des betriebssystemspezifischen Zeichen für einen Zeilenumbruch
-    String lineSeparator = System.getProperty("line.separator");
+    final String lineSeparator = System.getProperty("line.separator");
+
+    // Abrufen des Voreingestellten Separierungszeichens für die CSV-Datei aus den Properites
+    final String dSSeperator = PropertyHelper.getProperty("outputData.dataSetSeperator");
+
     // Defintion der Dateiendung
-    String fileExt = ".csv";
+    final String fileExt = ".csv";
+
     // zuweisen der Variante der Darstellung des Menueplans
-    // Abrufen der Werte aus Properties Datei
+    // Abrufen der Werte aus Properties
+    String layoutMenuePlan = PropertyHelper.getProperty("outputData.LayoutMenuePlan");
+
     // wenn Wert single --> dann zweispaltige Auflistung nach Namen
     // wenn Wert grouped --> dann pro Spalte bzw. Datum drei Gerichte
-    String layoutMenuePlan = PropertyHelper.getProperty("outputData.LayoutMenuePlan");
-    // Abrufen des Voreingestellten Seperierungszeichens für die CSV-Datei aus den Properites
-    String dSSeperator = PropertyHelper.getProperty("outputData.dataSetSeperator");
 
     /**
      * Standardkonstruktor
@@ -41,94 +46,136 @@ public class FileOutput implements IOutput {
 
     }
 
+    /**
+     * Diese Methode stellt die Möglichkeit bereit einn Speiseplan als
+     * CSV Ausgabe bereitzustellen. Die Ausgabe wird
+     * auf Basis der Daten eines Objekts {@link Canteen} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
+     * 
+     * @param canteen
+     *            Kantinen-Objekt wird übergeben
+     * @throws IOException
+     * @author Marcel Baxmann
+     */
     @Override
     public void outputMenuPlan(Canteen canteen) throws IOException {
         // auslesen des Kantinen-Namens
         String canteenName = canteen.getLocation().getName();
+        // typisieren von Variablen für Datensätze der Gerichte und Datum
         String meal;
         String date;
 
-        // Erzeugen String-Puffer Objekts in welches die Ergebnisse geschrieben werden
+        // Erzeugen StringBuilder-Objekts (Ausgabepuffer) in welches die Ergebnisse geschrieben werden
         StringBuilder ausgabeDaten = new StringBuilder();
 
         // List erstellen, in der die Gerichte nach Datum 1zu1 zugeordnet sind
         List<Meal> mealsSortedByDate = canteen.getMenuPlan().getMealsSortedByDate();
 
         // das Datum des Gültigkeitsbeginns auslesen - Verwendung für Dateiname
-        String startDate = shortendDate(mealsSortedByDate.get(0).getDate());
+        String startDate = FileHelper.shortendDate(mealsSortedByDate.get(0).getDate());
 
+        // Abfrage des Layouttyps - Springe in Ausführung wenn "single"
         if (layoutMenuePlan.equals("single")) {
-            // Überschriften Zeile einfügen
+            // Überschriften Zeile einfügen in Ausgabepuffer
             ausgabeDaten.append("Datum" + dSSeperator + "Gericht" + lineSeparator);
 
             // für jedes Gericht (meal) in der Liste werden das Datum und der Namen
-            // ausgelesen und in den String-Puffer angehangen
+            // ausgelesen und in den Ausgabepuffer angehangen
             for (Meal sortedMeal : mealsSortedByDate) {
 
-                date = shortendDate(sortedMeal.getDate());
+                date = FileHelper.shortendDate(sortedMeal.getDate());
                 meal = sortedMeal.getRecipe().getName();
 
                 ausgabeDaten.append(date + dSSeperator + meal + lineSeparator);
             }
+
+            // Ende Variante single und Abfrage für Layouttyp grouped
         }
         else if (layoutMenuePlan.equals("grouped")) {
-            // TODO (Marcel Baxmann) Kommentieren
+            // Abfrage der Anzahl der Gerichte pro Tag und speichern in lokale Variable (geparstes int)
             int mealsPerDay = Integer.parseInt(PropertyHelper.getProperty("planingPeriod.mealsPerDay"));
 
+            // Je nach Anzahl der Gerichte werden in den Ausgabepuffer die Überschriften Zeile angehangen
             ausgabeDaten.append("Datum" + dSSeperator);
             for (int i = 1; i < mealsPerDay + 1; i++) {
                 ausgabeDaten.append("Gericht " + i + dSSeperator);
             }
+            // Springe in nächste Zeile in Ausgabedatei
             ausgabeDaten.append(lineSeparator);
 
+            // Defintion einer Variable für die Abfrage, ob bereits ein Datum durchlaufen wurde
             String previousDate = null;
+
+            // für jedes Gericht (meal) in der Liste werden das Datum und der Namen ausgelesen
             for (Meal sortedMeal : mealsSortedByDate) {
-                date = shortendDate(sortedMeal.getDate());
+                date = FileHelper.shortendDate(sortedMeal.getDate());
                 meal = sortedMeal.getRecipe().getName();
 
+                // wenn previusDate noch nicht gefüllt ist (null) starte erste Zeile in Ausgabedatei
+                // indem erstes Datum und erstes Gericht in Ausgabepuffer angehangen werden
                 if (previousDate == null) {
                     ausgabeDaten.append(date + dSSeperator + meal);
+                    // setze Variable previousDate auf aktuelles Datum
                     previousDate = date;
                 }
+                // wenn previous Date bereits mit einem Datum gefüllt ist rufe folgenden Strang auf
                 else {
+                    // wenn vorhergehendes Datum aktuellem Datum entspricht haenge
+                    // aktuelles Gericht in gleiche Zeile in Ausgabepuffer an
                     if (date.equals(previousDate)) {
                         ausgabeDaten.append(dSSeperator + meal);
                     }
+                    // ansonsten
                     else {
+                        // haenge neue Zeile an und starte mit neuem Datum und erstem Gericht
                         ausgabeDaten.append(lineSeparator);
                         ausgabeDaten.append(date + dSSeperator + meal);
+                        // setze Variable previousDate auf aktuelles Datum
                         previousDate = date;
                     }
                 }
             }
         }
-
+        // sind alle Gerichte ausgelesen wird der Dateiname generiert
         String filename = FileHelper.generateFilename("Menueplan ab " + startDate + " - " + canteenName, fileExt);
+
+        // der Ausgabepuffer und der Dateiname werden an die Methode zum Schreiben in eine Datei übergeben
         FileHelper.ausgebenInDatei(ausgabeDaten.toString(), filename, true);
     }
 
     /**
-     * Klasse um ein Datum zu kürzen in das Format dd.mm.yyyy
+     * Diese Methode stellt die Möglichkeit bereit eine Einkaufsliste als
+     * CSV-Datei bereitzustellen. Die Ausgabe wird
+     * auf Basis der Daten eines Objekts {@link ShoppingList} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
      * 
-     * @param date
-     * @return String eines Datums im Format dd.mm.yyyy
+     * @param shoppingList
+     *            Einkaufslisten-Objekt wird übergeben
+     * @author Marcel Baxmann
      */
-    private String shortendDate(Date date) {
-        String stringDate;
-        DateFormat formatter = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        stringDate = formatter.format(date);
-        return stringDate;
-    }
-
     @Override
     public void outputShoppingList(ShoppingList shoppingList) {
+        // TODO (Marcel Baxmann) Ausgabe Shopping List als CSV
 
-        List<ShoppingListItem> shoppingListItems = shoppingList.getShoppingListItems();
+        Map<AbstractProvider, List<ShoppingListItem>> shoppingListItems = shoppingList
+                .getShoppingListItemsGroupedByProvider();
 
     }
 
+    /**
+     * Diese Methode stellt die Möglichkeit bereit eine Kostenübersicht als
+     * CSV-Datei bereitzustellen. Die Ausgabe wird
+     * auf Basis der Daten eines Objekts {@link ShoppingList} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
+     * 
+     * @param shoppingList
+     *            Einkaufslisten-Objekt wird übergeben
+     * @author Marcel Baxmann
+     */
     @Override
     public void outputTotalCosts(ShoppingList shoppingList) {
+        // TODO (Marcel Baxmann) Ausgabe Cost List als CSV
+
     }
 
 }
--- trunk/src/main/java/de/osjava/smartcanteen/output/HTMLOutput.java
+++ trunk/src/main/java/de/osjava/smartcanteen/output/HTMLOutput.java
@@ -1,8 +1,6 @@
 package de.osjava.smartcanteen.output;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
 
 import de.osjava.smartcanteen.builder.result.Meal;
@@ -12,17 +10,18 @@ import de.osjava.smartcanteen.helper.FileHelper;
 import de.osjava.smartcanteen.helper.PropertyHelper;
 
 /**
- * Die Klasse {@link HTMLOutput} ist eine Klasse, die das Interface {@link IOutput} implementiert und stellt daher die
- * Methoden die im Interface
- * definiert sind bereit. Die Ausgabe erfolgt als HTML-Seite.
+ * Die Klasse {@link HTMLOutput} ist eine Klasse, die das Interface {@link IOutput} implementiert
+ * und stellt daher die Methoden die im Interface definiert sind bereit.
+ * Die Ausgabe erfolgt in Dateien im Dateisystem als HTML. Der Pfad für den Ablageort
+ * wird in der zentralen Properties verwaltet.
  * 
  * @author Marcel Baxmann
  */
 public class HTMLOutput implements IOutput {
-    // auslesen des betriebssystemspezifischen Zeichen für einen Zeilenumbruch
-    String lineSeparator = System.getProperty("line.separator");
+    // Auslesen des betriebssystemspezifischen Zeichen für einen Zeilenumbruch
+    final String lineSeparator = System.getProperty("line.separator");
     // Defintion der Dateiendung
-    String fileExt = ".html";
+    final String fileExt = ".html";
 
     /**
      * Standardkonstruktor
@@ -30,6 +29,17 @@ public class HTMLOutput implements IOutput {
     public HTMLOutput() {
     }
 
+    /**
+     * Diese Methode stellt die Möglichkeit bereit einn Speiseplan als
+     * HTML-Datei bereitzustellen. Die Ausgabe wird
+     * auf Basis der Daten eines Objekts {@link Canteen} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
+     * 
+     * @param canteen
+     *            Kantinen-Objekt wird übergeben
+     * @throws IOException
+     * @author Marcel Baxmann
+     */
     @Override
     public void outputMenuPlan(Canteen canteen) throws IOException {
         // auslesen des Kantinen-Namens
@@ -42,7 +52,7 @@ public class HTMLOutput implements IOutput {
         List<Meal> mealsSortedByDate = canteen.getMenuPlan().getMealsSortedByDate();
 
         // das Datum des Gültigkeitsbeginns auslesen - Verwendung für Dateiname
-        String startDate = shortendDate(mealsSortedByDate.get(0).getDate());
+        String startDate = FileHelper.shortendDate(mealsSortedByDate.get(0).getDate());
 
         String meal;
         String date;
@@ -80,22 +90,22 @@ public class HTMLOutput implements IOutput {
 
         String previousDate = null;
         for (Meal sortedMeal : mealsSortedByDate) {
-            date = shortendDate(sortedMeal.getDate());
+            date = FileHelper.shortendDate(sortedMeal.getDate());
             meal = sortedMeal.getRecipe().getName();
 
             if (previousDate == null) {
                 ausgabeDaten.append("<td>" + date + "</td>");
-                ausgabeDaten.append("<td>" + meal + "</td>");
+                ausgabeDaten.append("<td>" + meal + pictureCheck(sortedMeal) + "</td>");
                 previousDate = date;
             }
             else {
                 if (date.equals(previousDate)) {
-                    ausgabeDaten.append("<td>" + meal + "</td>");
+                    ausgabeDaten.append("<td>" + meal + pictureCheck(sortedMeal) + "</td>");
                 }
                 else {
                     ausgabeDaten.append("</tr>" + lineSeparator +
                             "<td>" + date + "</td>");
-                    ausgabeDaten.append("<td>" + meal + "</td>");
+                    ausgabeDaten.append("<td>" + meal + pictureCheck(sortedMeal) + "</td>");
                     previousDate = date;
                 }
             }
@@ -110,18 +120,82 @@ public class HTMLOutput implements IOutput {
         FileHelper.ausgebenInDatei(ausgabeDaten.toString(), filename, true);
     }
 
-    private String shortendDate(Date date) {
-        String stringDate;
-        DateFormat formatter = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        stringDate = formatter.format(date);
-        return stringDate;
-    }
-
+    /**
+     * Diese Methode stellt die Möglichkeit bereit eine Einkaufsliste als
+     * HTML-Datei bereitzustellen. Die Ausgabe wird
+     * auf Basis der Daten eines Objekts {@link ShoppingList} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
+     * 
+     * @param shoppingList
+     *            Einkaufslisten-Objekt wird übergeben
+     * @author Marcel Baxmann
+     */
     @Override
     public void outputShoppingList(ShoppingList shoppingList) {
     }
 
+    /**
+     * Diese Methode stellt die Möglichkeit bereit eine Kostenübersicht als
+     * HTML-Datei bereitzustellen. Die Ausgabe wird
+     * auf Basis der Daten eines Objekts {@link ShoppingList} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
+     * 
+     * @param shoppingList
+     *            Einkaufslisten-Objekt wird übergeben
+     * @author Marcel Baxmann
+     */
     @Override
     public void outputTotalCosts(ShoppingList shoppingList) {
     }
+
+    /**
+     * Diese Methode stellt die Möglichkeit bereit das übergebene Gericht
+     * dahingehend zu ueberpruefen ob es Fisch, Fleisch oder vegetarisch ist
+     * Je nach Ergebnis wird ein anderer String mit Verweis auf ein passsendes Bild geliefert.
+     * Die Ausgabe wird auf Basis der Daten eines Objekts {@link Meal} gestaltet. Dieses
+     * Objekt muss der Methode beim Aufruf übergeben werden.
+     * 
+     * @param sortedMeal
+     *            ein Gericht
+     * @author Marcel Baxmann
+     */
+    private String pictureCheck(Meal sortedMeal) {
+        // TODO(Marcel) Pfadangabe für spätere Ergebnisse?
+        // TODO(Marcel Baxmann) Methode in FileHelper zum kopieren der Bilder in Ausgabeordner, sofern diese noch nicht
+        // exisiteren
+        // vordefinierte Bildergrößte wird aus Properties ausgelesen
+        int size = Integer.parseInt(PropertyHelper.getProperty("outputData.picSize"));
+        // auslesen der Pfadangabe
+        String picPath = PropertyHelper.getProperty("outputData.picSaveTo");
+
+        // Prüfe ob zu verwendendes Bild im Zielordner ist
+        boolean existFishPic = FileHelper.foundPicture(picPath + "fish.jpg");
+        boolean existMeatPic = FileHelper.foundPicture(picPath + "meat.jpg");
+        boolean existVeggiePic = FileHelper.foundPicture(picPath + "veggie.jpg");
+
+        // initalisieren des Rückgabewerts
+        String picture = "";
+        // wenn es ein Fischgericht ist und das Bild im angegebenen Pfad vorhanden ist wird ein HTML-Code
+        // mit Verweis auf ein Fischbild als String geliefert
+        if (sortedMeal.getRecipe().isFishRecipe() && existFishPic) {
+            picture = "<img src=\"" + picPath + "fish.jpg\" width=\"" + size + "\">";
+        }
+        // wenn es ein Fleischericht ist und das Bild im angegebenen Pfad vorhanden ist ein HTML-Code mit
+        // Verweis auf ein GFleischbild als String geliefert
+        else if (sortedMeal.getRecipe().isMeatRecipe() && existMeatPic) {
+            picture = "<img src=\"" + picPath + "meat.jpg\" width=\"" + size + "\">";
+        }
+        // wenn es ein vegetarisches Gericht und das Bild im angegebenen Pfad vorhanden ist wird ein HTML-Code
+        // mit Verweis auf ein Bild mit vegetarischem Essen als String geliefert
+        else if (sortedMeal.getRecipe().isVegetableRecipe() && existVeggiePic) {
+            picture = "<img src=\"" + picPath + "veggie.jpg\" width=\"" + size + "\">";
+        }
+        else {
+            // Wenn keine Zuordnung getroffen werden kann wird ein leerer String geliefert
+            picture = "";
+        }
+        // Rueckgabe des Strings
+        return picture;
+    }
+
 }
