edda748136e528f92c65d2cfda3b7ee639eac134
marcel.baxmann <marcel.baxmann@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2013-01-19 11:42:13 +0000

    - HTML-Ausgabe fuer Einkaufsliste fertiggestellt 	Feature: HTML-Liste an sich; zusatzliche Navigation

--- src/main/java/de/osjava/smartcanteen/output/HTMLOutput.java
+++ src/main/java/de/osjava/smartcanteen/output/HTMLOutput.java
@@ -2,9 +2,13 @@ package de.osjava.smartcanteen.output;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import de.osjava.smartcanteen.builder.result.Meal;
 import de.osjava.smartcanteen.builder.result.ShoppingList;
+import de.osjava.smartcanteen.builder.result.ShoppingListItem;
+import de.osjava.smartcanteen.data.AbstractProvider;
 import de.osjava.smartcanteen.data.Canteen;
 import de.osjava.smartcanteen.helper.FileHelper;
 import de.osjava.smartcanteen.helper.PropertyHelper;
@@ -139,10 +143,93 @@ public class HTMLOutput implements IOutput {
      * @param shoppingList
      *            Einkaufslisten-Objekt wird übergeben
      * @author Marcel Baxmann
+     * @throws IOException
      */
     @Override
-    public void outputShoppingList(ShoppingList shoppingList) {
-        // TODO (Marcel Baxmann) Methode implementieren
+    public void outputShoppingList(ShoppingList shoppingList) throws IOException {
+        // Erzeugen StringBuilder-Objekts (Ausgabepuffer) in welches die Ergebnisse geschrieben werden
+        StringBuilder outputBuffer = new StringBuilder();
+
+        // Map mit Key Abstract Provider und Value List<ShoppingListItem> anlegen
+        Map<AbstractProvider, List<ShoppingListItem>> shoppingListItems = shoppingList
+                .getShoppingListItemsGroupedByProvider();
+
+        // HTML-Ausgabe Generierung Kopfdaten des Dokuemnts und anhaengen in Puffer
+        outputBuffer.append(generateHTMLHeader("Einkaufsliste je Anbieter"));
+
+        // Generiere Übersicht mit Links auf Anker der einzelnen Tabellen
+        outputBuffer
+                .append("Einkaufslisten für folgende Lieferanten wurden generiert (anklicken zum anzeigen):<ul>" + lineSeparator);
+        // Alagen lokale Variable für die Positionbestimmung des Distributors
+        int numberDistributor = 0;
+        // für jeden Eintrag Abstract Provider Anweisungen ausfuehren
+        for (Entry<AbstractProvider, List<ShoppingListItem>> entry : shoppingListItems.entrySet()) {
+            String name = entry.getKey().getName();
+            outputBuffer
+                    .append("<li><a href=\"#" + numberDistributor + "\">" + name + "</a></li>" + lineSeparator);
+            // zum nächsten Distributor hochzahlen
+            numberDistributor++;
+        }
+        // Aufzaehlung beenden
+        outputBuffer.append("</ul>" + lineSeparator + lineSeparator);
+
+        // Zaehler für die Positionbestimmung des Distributors zuruecksetzen, da neue Schleife
+        numberDistributor = 0;
+        // prüfen, dass Objekt nicht null ist
+        if (shoppingListItems != null) {
+            // für jeden Eintrag Abstract Provider Anweisungen ausfuehren
+            for (Entry<AbstractProvider, List<ShoppingListItem>> entry : shoppingListItems.entrySet()) {
+                // Name des Anbieters auslesen
+                String name = entry.getKey().getName();
+
+                // HTML-Ausgabe Ueberschrift Lieferant mit HTML-Name Tag,
+                // so dass ueber die erstellte Liste direktzum Anbieter gesprungen werden kann
+                outputBuffer.append("<h2><a name=\"" + numberDistributor +
+                        "\">Einkaufsliste fuer Lieferant: " + name + "</a></h2>");
+
+                // HTML-Ausgabe Start Tabelle
+                outputBuffer.append("<table border=\"1\">" + lineSeparator + "<tr>");
+
+                // HTML-Ausgabe Beginn Überschriftzeile der Tabelle
+                outputBuffer.append("<th>Lieferant</th><th>Zutat</th>" +
+                        "<th>Menge</th><th>Einheit</th><th>Menge & Einheit</th></tr>");
+
+                // Liste mit Shoppinglist Items auslesen und in lokale Variable speichern
+                List<ShoppingListItem> value = entry.getValue();
+
+                // fuer jedes Listenelement Anweisungen ausfuehren - jeweils eine Zeile generieren
+                for (ShoppingListItem item : value) {
+                    // Name des Anbieters in Puffer anhaengen und Trennzeichen setzen
+                    outputBuffer.append("<tr><td>" + name + "</td>");
+                    // Name der Zutat in Puffer anhaengen und Trennzeichen setzen
+                    outputBuffer.append("<td>" + item.getIngredient().getName() + "</td>");
+
+                    // Wert und Einheit in getrennte Spalten
+                    // Wert in Puffer anhaengen und Trennzeichen setzen
+                    outputBuffer.append("<td>" + item.getQuantity().getValue() + "</td>");
+                    // Einheit in Puffer anhaengen und Trennzeichen setzen
+                    outputBuffer.append("<td>" + item.getQuantity().getUnit().getName() + "</td>");
+
+                    // Wert und Einheit in gleiche Spalte schreiben
+                    // Wert in Puffer anhaengen und Trennzeichen setzen
+                    outputBuffer.append("<td>" + item.getQuantity().getValue() + " ");
+                    // Einheit in Puffer anhaengen und Trennzeichen setzen
+                    outputBuffer.append(item.getQuantity().getUnit().getName() + "</td></tr>" + lineSeparator);
+                }
+                // HTML-Ausgabe Ende Tabelle mit Link zum Seitenbeginn
+                outputBuffer
+                        .append("</table>" + lineSeparator + "<a href=\"#top\"> zurueck zur Uebersicht</a>" + lineSeparator);
+                numberDistributor++;
+            }
+        }
+        // HTML-Ausgabe Ende Tabelle und HTML-Dokument
+        outputBuffer.append("</body>" + lineSeparator + "</html>");
+
+        // sind alle Gerichte ausgelesen wird der Dateiname generiert
+        String filename = FileHelper.generateFilename("Einkaufsliste", fileExt);
+
+        // der Ausgabepuffer und der Dateiname werden an die Methode zum Schreiben in eine Datei übergeben
+        FileHelper.ausgebenInDatei(outputBuffer.toString(), filename, true);
     }
 
     /**
@@ -231,7 +318,7 @@ public class HTMLOutput implements IOutput {
                 "<body>" + lineSeparator);
 
         // HTML-Ausgabe Überschrift
-        buffer.append("<h1>" + title + "</h1>");
+        buffer.append("<h1><a name=\"top\"><u>" + title + "</u></a></h1>" + lineSeparator);
 
         return buffer.toString();
     }
