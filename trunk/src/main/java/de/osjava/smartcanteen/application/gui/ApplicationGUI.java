2cbb6da35df3133544b215ed247e0cd0ddfcaa84
marcel.baxmann <marcel.baxmann@a7985a38-d21d-8318-15bf-eb47bbcb9d61>
2013-01-20 17:56:24 +0000

    - Benutzeroberflachen Design optimiert  - Optionsflaeche und Ausgabeflaeche mit Tabs  - Verarbeitung starten funktionsfaehig  - Ergebnisse nach korrekter Verarbeitung starten ist funktionsfaehig

--- trunk/src/main/java/de/osjava/smartcanteen/application/gui/ApplicationGUI.java
+++ trunk/src/main/java/de/osjava/smartcanteen/application/gui/ApplicationGUI.java
@@ -1,5 +1,7 @@
 package de.osjava.smartcanteen.application.gui;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.Toolkit;
@@ -9,10 +11,17 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 
 import de.osjava.smartcanteen.application.Application;
 
@@ -32,8 +41,12 @@ import de.osjava.smartcanteen.application.Application;
  * @author Marcel Baxmann
  */
 public class ApplicationGUI {
-    int windowWidth = 300; // Angabe der Breite des Fensters
-    int windowHeight = 500; // Angabe der Höhe des Fensters
+    int windowWidth = 800; // Angabe der Breite des Fensters
+    int windowHeight = 400; // Angabe der Höhe des Fensters
+    int procentOptionPane = 65;
+    int procentOutputPane = 35;
+    private Application application;
+    boolean finishedProcess = false;
 
     /**
      * Standardkonstruktor
@@ -42,33 +55,112 @@ public class ApplicationGUI {
      * @param shoppingList
      */
     public ApplicationGUI(final Application application) throws IOException {
+        this.application = application;
+    }
+
+    public void initialize() {
+        // TODO (Marcel Baxmann)
+
+        // Anlegen des Rahmens für die Anzeige
         JFrame frame = new JFrame("SmartCanteen");
         frame.setSize(windowWidth, windowHeight);
-        // frame.setLocation(getDisplayCenter());
+        frame.setLocation(getDisplayCenter());
+        frame.setDefaultCloseOperation(frame.DO_NOTHING_ON_CLOSE);
         frame.addWindowListener(new DialogWindowClosingListener());
-        frame.setLayout(new GridLayout(5, 1));
-        frame.setVisible(true);
-        frame.toFront();
+        frame.setLayout(new GridLayout());
 
-        JLabel input = new JLabel("Input-Dateien:");
-        frame.add(input);
+        // Aufteilen des Anzeigerahmens in Rechts (Output) und Links (Optionen)
+        // Optionsbereichs
+        JPanel pnlOptionArea = new JPanel();
+        pnlOptionArea.setLayout(new GridLayout(3, 1));
+        pnlOptionArea.setBorder(BorderFactory.createTitledBorder("Optionen"));
+        frame.add(pnlOptionArea);
 
-        JLabel process = new JLabel("Verarbeitungs-Einstellungen:");
-        frame.add(process);
+        // Ausagbebereich --> noch zu bauen
+        JPanel pnlOutputArea = new JPanel();
+        pnlOutputArea.setBorder(BorderFactory.createTitledBorder("Voransicht:"));
+        // Layout to be defined
+        pnlOutputArea.setLayout(new GridLayout());
+        frame.add(pnlOutputArea);
 
-        JLabel output = new JLabel("Ausgabe-Einstellungen:");
-        frame.add(output);
+        JTabbedPane tabPaneOutput = new JTabbedPane();
+        tabPaneOutput.addTab("Menue Plan", null);
+        tabPaneOutput.addTab("Kosten Plan", null);
+        tabPaneOutput.addTab("Einkaufs Plan", null);
+        pnlOutputArea.add(tabPaneOutput);
 
-        JButton startProcess = new JButton("Verarbeitung starten");
-        startProcess.setEnabled(true);
-        frame.add(startProcess);
-        startProcess.addActionListener(new ActionListener() {
+        // Unterteilen des Optionsbereich in die Kategorien Input-Option, Process-Option und Output-Option
+        // Input-Option
+        JPanel pnlInputOptionArea = new JPanel();
+        pnlInputOptionArea.setLayout(new GridLayout(2, 1));
+        pnlInputOptionArea.setBorder(BorderFactory.createTitledBorder("Eingabe-Dateien"));
+        pnlOptionArea.add(pnlInputOptionArea);
+
+        // Process-Option
+        JPanel pnlProcessOptionArea = new JPanel();
+        pnlProcessOptionArea.setLayout(new FlowLayout());
+        pnlProcessOptionArea.setBorder(BorderFactory.createTitledBorder("Verarbeitungs-Einstellungen"));
+        pnlOptionArea.add(pnlProcessOptionArea);
+
+        // Output-Option
+        JPanel pnlOutputOptionArea = new JPanel();
+        pnlOutputOptionArea.setLayout(new FlowLayout());
+        pnlOutputOptionArea.setBorder(BorderFactory.createTitledBorder("Ausgabe-Einstellungen"));
+        pnlOptionArea.add(pnlOutputOptionArea);
+
+        // Füllen des Bereichs Input-Option mit Content und Einstellmöglichkeiten
+        JLabel lblInputText = new JLabel("Folgende Dateien wurden in das Programm geladen:");
+        pnlInputOptionArea.add(lblInputText);
+
+        JTextArea displayInputFiles = new JTextArea(application.getInputFiles(), 2, 1);
+        pnlInputOptionArea.add(displayInputFiles);
+        displayInputFiles.setLineWrap(true);
+        displayInputFiles.setFocusable(false);
+
+        // Füllen des Bereichs Process-Option mit Content und Einstellmöglichkeiten
+        JLabel lblProcessText = new JLabel("Verarbeitungs-Art einstellen:");
+        pnlProcessOptionArea.add(lblProcessText);
+
+        ButtonGroup rbtnProcessTypeGroup = new ButtonGroup();
+        // JPanel rbtnPanel = new JPanel();
+        // rbtnPanel.setLayout(new FlowLayout());
+
+        JRadioButton rbtnProcessType1 = new JRadioButton("Zufall");
+        JRadioButton rbtnProcessType2 = new JRadioButton("Sequentiell");
+        rbtnProcessTypeGroup.add(rbtnProcessType1);
+        rbtnProcessTypeGroup.add(rbtnProcessType2);
+        // rbtnPanel.add(rbtnProcessType1);
+        // rbtnPanel.add(rbtnProcessType2);
+        // pnlProcessOptionArea.add(rbtnPanel);
+        pnlProcessOptionArea.add(rbtnProcessType1);
+        pnlProcessOptionArea.add(rbtnProcessType2);
+
+        final JButton btnStartProcess = new JButton("Verarbeitung starten");
+        btnStartProcess.setEnabled(true);
+        pnlProcessOptionArea.add(btnStartProcess);
+
+        // Füllen des Bereichs Output-Option mit Content und Einstellmöglichkeiten
+        JLabel llbOutputText = new JLabel("Ausgabeformat festlegen:");
+        pnlOutputOptionArea.add(llbOutputText);
+        JCheckBox cboxOutputFormat1 = new JCheckBox("CSV:");
+        pnlOutputOptionArea.add(cboxOutputFormat1);
+        JCheckBox cboxOutputFormat2 = new JCheckBox("HTML:");
+        pnlOutputOptionArea.add(cboxOutputFormat2);
+
+        final JButton btnSaveResults = new JButton("Speichern der Ergebnisse");
+        pnlOutputOptionArea.add(btnSaveResults);
+        btnSaveResults.setEnabled(false);
+
+        btnStartProcess.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
                     application.buildMenuePlan();
                     application.buildShoppingList();
+                    finishedProcess = true;
+                    btnSaveResults.setEnabled(true);
+                    btnStartProcess.setText("Neu berechnen");
                     System.out.println("Erstellung Plaene erfolgreich");
                 } catch (IOException e1) {
                     // TODO(Marcel) handle this exception properly
@@ -79,48 +171,57 @@ public class ApplicationGUI {
             }
         });
 
-        JButton saveResults = new JButton("Speichern der Ergebnisse");
-        frame.add(saveResults);
-        saveResults.addActionListener(new ActionListener() {
+        btnSaveResults.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
                     application.outputApplicationResult();
                     System.out.println("ok");
+
                 } catch (IOException e1) {
                     // TODO(Marcel) handle this exception properly
                     e1.printStackTrace();
                     System.out.println("not");
                 }
-                // Application.outputApplicationResult(shoppingList, canteens);
 
             }
         });
 
+        frame.setVisible(true);
+        frame.toFront();
     }
 
     /**
-     * ermittelt die Bildschirmmitte anhand der Auflösung
+     * ermittelt die Bildschirmmitte anhand der Auflösung und gibt diese als Point zurück
      * 
      * @return Point
      *         der Bildschrimmitte
      */
-    // TODO (Marcel) Position korrgieren
     private Point getDisplayCenter() {
+
+        Dimension display = Toolkit.getDefaultToolkit().getScreenSize();
+
         Point displayCenter = new Point(0, 0);
-        displayCenter.x = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - windowWidth) / 2;
-        displayCenter.y = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() - windowWidth) / 2;
+        displayCenter.x = (int) ((display.getWidth() - windowWidth) / 2);
+        displayCenter.y = (int) ((display.getHeight() - windowHeight) / 2);
+
         return displayCenter;
     }
 
+    /**
+     * Inner-Class zum Abfragen des Fenster-Schließ-Events
+     */
     public class DialogWindowClosingListener extends WindowAdapter {
         @Override
         public void windowClosing(WindowEvent event)
         {
-            int option = JOptionPane.showConfirmDialog(null, "Applikation beenden?");
-            if (option == JOptionPane.OK_OPTION)
+            int option = JOptionPane.showConfirmDialog(null, "Möchten Sie die Applikation beenden?", "Beenden",
+                    JOptionPane.OK_OPTION);
+            if (option == JOptionPane.OK_OPTION) {
                 System.exit(0);
+            }
         }
     }
+
 }
